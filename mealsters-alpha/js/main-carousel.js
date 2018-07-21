054f70a5b72b3c4cb8b9fc8a3410876945597202
Felipe <felipe.a@msn.com>
2018-07-21 09:34:16 -0700

    adding SASS and the main caorusel

---
+++ mealsters-alpha/js/main-carousel.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    $('.your-class').slick({
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        mobileFirst: true,
+        edgeFriction: 0,
+        speed: 100,
+        arrows: false,
+
+    });
+});
