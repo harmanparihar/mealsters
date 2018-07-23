9d9cba911f47cb78ceac4325c009c270838104d2
Felipe <felipe.a@msn.com>
2018-07-22 23:51:00 -0700

    adding more animations and JS to link main and info

--- mealsters-alpha/js/main-carousel.js
+++ mealsters-alpha/js/main-carousel.js
@@ -1,5 +1,7 @@
 $(function() {
 
+
+
     $('.fooditem').slick({
         centerMode: true,
         centerPadding: '40px',
@@ -14,16 +16,74 @@ $(function() {
 
 
 
-    $('#foodInfo').on('click', function() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $('#gofoodinfo').on('click', function() {
+
+
+
+
+        let userChoice = $(".slick-active>div>div>img").attr("src");
+        console.log(userChoice)
+
+        $('head').append(
+            `<style>
+                .foodinfo-image {
+                    background: linear-gradient(to bottom, rgba(0, 0, 0, .7), transparent), url("${userChoice}");
+                    background-position: center;
+                }
+            </style>
+        `)
+
+        let userChoiceTitle = $(".slick-active>div>div>h2").text();
+        $(".foodinfo > h2").text(userChoiceTitle);
+
+
+
         // console.log("hey you");
-        // setTimeout(function() {
-        //     $('.main-nav-bottom').addClass('toInfo');
-        // }, 100);
+
+        $('.fooditem').addClass('toInfo-2');
+        $('#name-main').addClass('toInfo-2');
+        setTimeout(function() {
+            $('.main-nav-bottom').addClass('toInfo');
+            $("#menu").addClass('menu-to-info');
+            $("#goback").addClass('goback-to-info');
+            $('#goback').css("display", "inline-block");
+        }, 100);
 
         setTimeout(function() {
             $('.main-section').css("display", "none");
-        }, 300);
+            $('#name-main').css("opacity", "0");
+        }, 250);
 
+        setTimeout(function() {
+            $('.foodinfo').css("display", "block")
+            $('.foodinfo').addClass("foodinfo-anim-01")
+        }, 250);
+
+        setTimeout(function() {
+            $("#infobox1").addClass("foodinfo-anim-02")
+            $('#infobox1').css("opacity", "1")
+        }, 450);
+        setTimeout(function() {
+            $("#infobox2").addClass("foodinfo-anim-02")
+            $('#infobox2').css("opacity", "1")
+        }, 550);
+        setTimeout(function() {
+            $("#infobox3").addClass("foodinfo-anim-02")
+            $('#infobox3').css("opacity", "1")
+        }, 650);
     })
 
 
